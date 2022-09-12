fd39e80ca24cd25251afb2acd9548410eb408ea9
Baricho <ekabue0@gmail.com>
2022-09-12 11:29:17 +0300

    Edit layout

--- src/components/Call-to-action/index.jsx
+++ src/components/Call-to-action/index.jsx
@@ -1,23 +1,23 @@
-import React from "react";
-import Split from "../Split";
-import Link from "next/link";
+import React from 'react';
+import Split from '../Split';
+import Link from 'next/link';
 
 const CallToAction = ({ img, theme, subBG }) => {
   return (
     <section
-      className={`call-action section-padding ${subBG ? "sub-bg" : ""} bg-img`}
-      style={{ backgroundImage: `url(${img ? img : "/img/pattern.png"})` }}
+      className={`call-action section-padding ${subBG ? 'sub-bg' : ''} bg-img`}
+      style={{ backgroundImage: `url(${img ? img : '/img/pattern.png'})` }}
     >
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8 col-lg-9">
-            <div className="content sm-mb30">
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-8 col-lg-9'>
+            <div className='content sm-mb30'>
               <Split>
-                <h6 className="wow words chars splitting" data-splitting>
+                <h6 className='wow words chars splitting' data-splitting>
                   Let&apos;s Talk
                 </h6>
                 <h2
-                  className="wow words custom-font chars splitting"
+                  className='wow words custom-font chars splitting'
                   data-splitting
                 >
                   about your <b>next project</b>.
@@ -26,13 +26,13 @@ const CallToAction = ({ img, theme, subBG }) => {
             </div>
           </div>
 
-          <div className="col-md-4 col-lg-3 valign">
-            <Link href="/contact/contact-dark">
+          <div className='col-md-4 col-lg-3 valign'>
+            <Link href='/contact/contact-dark'>
               <a
                 className={`btn-curve ${
-                  theme == "light" ? "btn-blc" : "btn-lit"
+                  theme == 'light' ? 'btn-blc' : 'btn-lit'
                 } wow fadeInUp`}
-                data-wow-delay=".5s"
+                data-wow-delay='.5s'
               >
                 <span>Get In Touch</span>
               </a>
--- src/components/Portfolio-custom-column/index.jsx
+++ src/components/Portfolio-custom-column/index.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
-import Split from "../Split";
-import Link from "next/link";
-import initIsotope from "../../common/initIsotope";
-import portfolio1Data from "../../data/sections/portfolio1.json";
+import React from 'react';
+import Split from '../Split';
+import Link from 'next/link';
+import initIsotope from '../../common/initIsotope';
+import portfolio1Data from '../../data/sections/portfolio1.json';
 
 const PortfolioCustomColumn = ({
   column,
@@ -21,80 +21,80 @@ const PortfolioCustomColumn = ({
     }
   }, [pageLoaded]);
   return (
-    <section className="portfolio section-padding pb-70">
+    <section className='portfolio section-padding pb-70'>
       {!hideSectionTitle && (
-        <div className="container">
-          <div className="sec-head custom-font">
-            <h6 className="wow fadeIn" data-wow-delay=".5s">
+        <div className='container'>
+          <div className='sec-head custom-font text-center'>
+            <h6 className='wow fadeIn' data-wow-delay='.5s'>
               Portfolio
             </h6>
             <Split>
-              <h3 className="wow words chars splitting" data-splitting>
-                Our Works.
+              <h3 className='wow words chars splitting' data-splitting>
+                Solved challenges
               </h3>
             </Split>
-            <span className="tbg text-right">Portfolio</span>
+            <span className='tbg '>Portfolio</span>
           </div>
         </div>
       )}
 
-      <div className={`${column === 3 ? "container-fluid" : "container"}`}>
-        <div className="row">
+      <div className={`${column === 3 ? 'container-fluid' : 'container'}`}>
+        <div className='row'>
           {!hideFilter && (
             <div
               className={`filtering ${
-                filterPosition === "center"
-                  ? "text-center"
-                  : filterPosition === "left"
-                  ? "text-left"
-                  : "text-right"
+                filterPosition === 'center'
+                  ? 'text-center'
+                  : filterPosition === 'left'
+                  ? 'text-left'
+                  : 'text-right'
               } col-12`}
             >
-              <div className="filter">
-                <span data-filter="*" className="active">
+              <div className='filter'>
+                <span data-filter='*' className='active'>
                   All
                 </span>
-                <span data-filter=".brand">Branding</span>
-                <span data-filter=".web">Mobile App</span>
-                <span data-filter=".graphic">Creative</span>
+                <span data-filter='.brand'>Branding</span>
+                <span data-filter='.web'>Mobile App</span>
+                <span data-filter='.graphic'>Creative</span>
               </div>
             </div>
           )}
 
-          <div className="gallery full-width">
+          <div className='gallery full-width'>
             {portfolio1Data.map((item, index) => (
               <div
                 key={item.id}
                 className={`${
                   column === 3
-                    ? "col-lg-4 col-md-6"
+                    ? 'col-lg-4 col-md-6'
                     : column === 2
-                    ? "col-md-6"
-                    : "col-12"
+                    ? 'col-md-6'
+                    : 'col-12'
                 } items ${item.filterCategory} wow fadeInUp ${
                   item.id === 2 && column == 3
-                    ? "lg-mr"
+                    ? 'lg-mr'
                     : item.id === 1 && column == 2
-                    ? "lg-mr"
-                    : ""
+                    ? 'lg-mr'
+                    : ''
                 }`}
-                data-wow-delay=".4s"
+                data-wow-delay='.4s'
               >
-                <div className="item-img">
-                  <Link href="/project-details2/project-details2-dark">
-                    <a className="imago wow">
-                      <img src={item.image} alt="image" />
-                      <div className="item-img-overlay"></div>
+                <div className='item-img'>
+                  <Link href='/project-details2/project-details2-dark'>
+                    <a className='imago wow'>
+                      <img src={item.image} alt='image' />
+                      <div className='item-img-overlay'></div>
                     </a>
                   </Link>
                 </div>
-                <div className="cont">
+                <div className='cont'>
                   <h6>{item.title}</h6>
                   <span>
                     {item.tags.map((tag, index) => (
                       <React.Fragment key={index * 3}>
-                        <Link href="/works4/works4-dark">{tag}</Link>
-                        {index == item.tags.length - 1 ? "" : ","}
+                        <Link href='/works4/works4-dark'>{tag}</Link>
+                        {index == item.tags.length - 1 ? '' : ','}
                       </React.Fragment>
                     ))}
                   </span>
