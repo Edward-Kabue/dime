e85cfbd87ad0c8c30c6018572b4b09a69e8c49fd
Baricho <ekabue0@gmail.com>
2022-09-12 12:02:32 +0300

    updates

--- src/pages/about/about-light.jsx
+++ src/pages/about/about-light.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import AboutHeader from "../../components/About-header";
-import AboutIntro from "../../components/About-intro";
-import CallToAction from "../../components/Call-to-action";
-import Clients1 from "../../components/Clients1";
-import Footer from "../../components/Footer";
-import MinimalArea from "../../components/Minimal-area";
-import Navbar from "../../components/Navbar";
-import Services4 from "../../components/Services4";
-import SkillsCircle from "../../components/Skills-circle";
-import Team1 from "../../components/Team1";
-import VideoWithTestimonials from "../../components/Video-with-testimonials";
-import LightTheme from "../../layouts/Light";
-import appData from '../../data/app.json'
+import React from 'react';
+import AboutHeader from '../../components/About-header';
+import AboutIntro from '../../components/About-intro';
+import CallToAction from '../../components/Call-to-action';
+import Clients1 from '../../components/Clients1';
+import Footer from '../../components/Footer';
+import MinimalArea from '../../components/Minimal-area';
+import Navbar from '../../components/Navbar';
+import Services4 from '../../components/Services4';
+import SkillsCircle from '../../components/Skills-circle';
+import Team1 from '../../components/Team1';
+import VideoWithTestimonials from '../../components/Video-with-testimonials';
+import LightTheme from '../../layouts/Light';
+import appData from '../../data/app.json';
 
 const AboutDark = () => {
   const navbarRef = React.useRef(null);
@@ -20,17 +20,17 @@ const AboutDark = () => {
     var navbar = navbarRef.current,
       logo = logoRef.current;
     if (window.pageYOffset > 300) {
-      navbar.classList.add("nav-scroll");
+      navbar.classList.add('nav-scroll');
     } else {
-      navbar.classList.remove("nav-scroll");
+      navbar.classList.remove('nav-scroll');
     }
-    window.addEventListener("scroll", () => {
+    window.addEventListener('scroll', () => {
       if (window.pageYOffset > 300) {
-        navbar.classList.add("nav-scroll");
-        logo.setAttribute("src", appData.darkLogo);
+        navbar.classList.add('nav-scroll');
+        logo.setAttribute('src', appData.darkLogo);
       } else {
-        navbar.classList.remove("nav-scroll");
-        logo.setAttribute("src", appData.lightLogo);
+        navbar.classList.remove('nav-scroll');
+        logo.setAttribute('src', appData.lightLogo);
       }
     });
   }, [navbarRef]);
@@ -40,13 +40,13 @@ const AboutDark = () => {
       <AboutHeader />
       <AboutIntro />
       <Services4 withPadding withOutTitle />
-      <VideoWithTestimonials />
-      <SkillsCircle subBG theme="light" />
+      {/* <VideoWithTestimonials /> */}
+      {/* <SkillsCircle subBG theme="light" /> */}
       <Team1 />
       <MinimalArea />
-      <Clients1 theme="light" />
-      <CallToAction theme="light" subBG />
-      <Footer />
+      <Clients1 theme='light' />
+      <CallToAction theme='light' subBG />
+      {/* <Footer /> */}
     </LightTheme>
   );
 };
