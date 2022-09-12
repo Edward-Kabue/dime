7ff205d0780b513f0fc358f7928f5932924bf839
Baricho <ekabue0@gmail.com>
2022-09-12 17:20:34 +0300

    Edit works section

--- src/components/About-header/index.jsx
+++ src/components/About-header/index.jsx
@@ -1,21 +1,21 @@
-import React from "react";
+import React from 'react';
 
 const AboutHeader = () => {
   return (
     <header
-      className="pages-header bg-img valign parallaxie"
-      style={{ backgroundImage: "url(/img/slid/1.jpg)" }}
-      data-overlay-dark="5"
+      className='pages-header bg-img valign parallaxie'
+      style={{ backgroundImage: 'url(/img/slid/1.jpg)' }}
+      data-overlay-dark='5'
     >
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="cont text-center">
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-12'>
+            <div className='cont text-center'>
               <h1>About Us</h1>
-              <div className="path">
-                <a href="#0">Home</a>
+              <div className='path'>
+                <a href='#0'>Home</a>
                 <span>/</span>
-                <a href="#0" className="active">
+                <a href='#0' className='active'>
                   About Us
                 </a>
               </div>
