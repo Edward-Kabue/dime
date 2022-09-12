dabf5c93fa1c6732dd628e9e889093b499f0b2f9
Baricho <ekabue0@gmail.com>
2022-09-12 12:45:53 +0300

    Client refactor

--- src/components/Clients-brands/index.jsx
+++ src/components/Clients-brands/index.jsx
@@ -1,20 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Split from "../Split";
-import ClientsBrandsData from "../../data/sections/clients-brands.json";
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Split from '../Split';
+import ClientsBrandsData from '../../data/sections/clients1.json';
 
 const ClientsBrands = ({ subBG, theme }) => {
   return (
     <section
-      className={`clients-brand section-padding ${subBG ? "sub-bg" : ""}`}
+      className={`clients-brand section-padding ${subBG ? 'sub-bg' : ''}`}
     >
-      <div className="container">
+      <div className='container'>
         <Slider
-          className=""
-          data-wow-delay=".5s"
+          className=''
+          data-wow-delay='.5s'
           {...{
             dots: false,
             infinite: true,
@@ -26,7 +26,7 @@ const ClientsBrands = ({ subBG, theme }) => {
               {
                 breakpoint: 1024,
                 settings: {
-                  slidesToShow: 4,
+                  slidesToShow: 5,
                 },
               },
               {
@@ -45,43 +45,39 @@ const ClientsBrands = ({ subBG, theme }) => {
           }}
         >
           {ClientsBrandsData.map((item, index) => (
-            <div className="brands" key={item.id}>
-              <h2 style={{ display: "none" }}> &nbsp; </h2>
+            <div className='brands' key={item.id}>
+              <h2 style={{ display: 'none' }}> &nbsp; </h2>
               <div
-                className="item"
+                className='item'
                 data-wow-delay={
-                  index + 1 == "1"
-                    ? ".3s"
-                    : index + 1 == "2"
-                    ? ".6s"
-                    : index + 1 == "3"
-                    ? ".8s"
-                    : index + 1 == "4"
-                    ? ".3s"
-                    : index + 1 == "5"
-                    ? ".4s"
-                    : index + 1 == "6"
-                    ? ".7s"
-                    : index + 1 == "7"
-                    ? ".5s"
-                    : index + 1 == "8"
-                    ? ".3s"
-                    : ""
+                  index + 1 == '1'
+                    ? '.3s'
+                    : index + 1 == '2'
+                    ? '.6s'
+                    : index + 1 == '3'
+                    ? '.8s'
+                    : index + 1 == '4'
+                    ? '.3s'
+                    : index + 1 == '5'
+                    ? '.4s'
+                    : index + 1 == '6'
+                    ? '.2s'
+                    : ''
                 }
               >
-                <div className="img">
+                <div className='img'>
                   <img
-                    src={theme == "dark" ? item.darkImage : item.lightImage}
-                    alt=""
+                    src={theme == 'dark' ? item.darkImage : item.lightImage}
+                    alt=''
                   />
                 </div>
                 <Split>
                   <a
-                    href="#0"
-                    className="link words chars splitting"
+                    href={item.url}
+                    className='link words chars splitting'
                     data-splitting
                   >
-                    www.avo.com
+                    {item.url}
                   </a>
                 </Split>
               </div>
