796f7901250c871e8e259d155e7816b9634d6824
Baricho <ekabue0@gmail.com>
2022-09-14 10:30:52 +0300

    Add cta

--- src/components/About-header/index.jsx
+++ src/components/About-header/index.jsx
@@ -1,24 +1,19 @@
 import React from 'react';
+import Split from '../Split';
 
 const AboutHeader = () => {
   return (
-    <header
-      className='pages-header bg-img valign parallaxie'
-      style={{ backgroundImage: 'url(/img/slid/1.jpg)' }}
-      data-overlay-dark='5'
-    >
+    <header className='pages-header bg-img valign parallaxie'>
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12'>
-            <div className='cont text-center'>
-              <h1>About Us</h1>
-              <div className='path'>
-                <a href='#0'>Home</a>
-                <span>/</span>
-                <a href='#0' className='active'>
-                  About Us
-                </a>
-              </div>
+            <div className='sec-head custom-font text-center'>
+              <Split>
+                <h3 className='wow words chars splitting' data-splitting>
+                  About us
+                </h3>
+              </Split>
+              <span className='tbg'>About us</span>
             </div>
           </div>
         </div>
