8be1b0f0e32cec7078c0de654bf5e762f916662f
kemoeverlyne <kemoeverlyne@gmail.com>
2022-09-16 09:03:53 +0300

    technology Service

--- src/pages/services/techServices.jsx
+++ src/pages/services/techServices.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import AboutHeader from '../../components/About-header';
-import AboutIntro from '../../components/About-intro';
+// import AboutIntro from '../../components/About-intro';
 import CallToAction from '../../components/Call-to-action';
 import Clients1 from '../../components/Clients1';
 import Footer from '../../components/Footer';
 import MinimalArea from '../../components/Minimal-area';
 import Navbar from '../../components/Navbar';
 
-import SkillsCircle from '../../components/Skills-circle';
-import Team1 from '../../components/Team1';
-import VideoWithTestimonials from '../../components/Video-with-testimonials';
+// import SkillsCircle from '../../components/Skills-circle';
+// import Team1 from '../../components/Team1';
+// import VideoWithTestimonials from '../../components/Video-with-testimonials';
 import LightTheme from '../../layouts/Light';
 import appData from '../../data/app.json';
 import Services2 from '../../components/Services2';
+import ServiceHeader from '../../components/Service-header/index';
 
 const AboutDark = () => {
   const navbarRef = React.useRef(null);
@@ -38,9 +39,10 @@ const AboutDark = () => {
   return (
     <LightTheme>
       <Navbar nr={navbarRef} lr={logoRef} />
-
+      <ServiceHeader />
+      <div className='main-content'>
       <Services2 />
-
+      </div>
       <CallToAction theme='light' subBG />
       {/* <Footer /> */}
     </LightTheme>
