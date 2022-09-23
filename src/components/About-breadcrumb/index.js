94240a0d2a82b90bcfcf9c082aea51d244ecca29
Baricho <ekabue0@gmail.com>
2022-09-23 09:55:40 +0300

    edit breadcrumb

--- src/components/About-breadcrumb/index.js
+++ src/components/About-breadcrumb/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from 'react'
 
 const AboutBreadcrumb = ({ sliderRef }) => {
   return (
     <header
       ref={sliderRef}
-      className='works-header fixed-slider hfixd valign'
-      style={{ backgroundImage: 'url(/img/slid/1.jpg)' }}
-      data-overlay-dark='5'
+      className="works-header fixed-slider hfixd valign"
+      style={{ backgroundImage: 'url(/img/slid/1.png)' }}
+      data-overlay-dark="5"
     >
-      <div className='container'>
-        <div className='row'>
-          <div className='col-lg-12'>
-            <div className='cont text-center'>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="cont text-center">
               <h1>About Us</h1>
-              <div className='path'>
-                <a href='#0'>Home</a>
+              <div className="path">
+                <a href="#0">Home</a>
                 <span>/</span>
-                <a href='#0' className='active'>
+                <a href="#0" className="active">
                   About Us
                 </a>
               </div>
@@ -25,7 +25,7 @@ const AboutBreadcrumb = ({ sliderRef }) => {
         </div>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default AboutBreadcrumb;
+export default AboutBreadcrumb
--- src/components/Video-with-testimonials/index.jsx
+++ src/components/Video-with-testimonials/index.jsx
@@ -1,16 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import ModalVideo from 'react-modal-video';
-import 'react-modal-video/css/modal-video.css';
+import React from 'react'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import ModalVideo from 'react-modal-video'
+import 'react-modal-video/css/modal-video.css'
 
 const VideoWithTestimonials = () => {
-  const [isOpen, setOpen] = React.useState(false);
+  const [isOpen, setOpen] = React.useState(false)
   React.useEffect(() => {
-    console.clear();
-  }, []);
+    console.clear()
+  }, [])
   const settings = {
     dots: true,
     infinite: true,
@@ -18,67 +18,67 @@ const VideoWithTestimonials = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-  };
+  }
   return (
-    <section className='block-sec'>
+    <section className="block-sec">
       <div
-        className='background bg-img section-padding pb-0'
-        style={{ backgroundImage: `url(/img/slid/1.jpg)` }}
-        data-overlay-dark='8'
+        className="background bg-img section-padding pb-0"
+        style={{ backgroundImage: `url(/img/slid/1.png)` }}
+        data-overlay-dark="8"
       >
-        <div className='container'>
-          <div className='row'>
-            <div className='col-lg-6'>
-              <div className='vid-area'>
-                <div className='vid-icon'>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="vid-area">
+                <div className="vid-icon">
                   {typeof window !== 'undefined' && (
                     <ModalVideo
-                      channel='vimeo'
+                      channel="vimeo"
                       autoplay
                       isOpen={isOpen}
-                      videoId='127203262'
+                      videoId="127203262"
                       onClose={() => setOpen(false)}
                     />
                   )}
                   <a
-                    className='vid'
+                    className="vid"
                     onClick={(e) => {
-                      e.preventDefault();
-                      setOpen(true);
+                      e.preventDefault()
+                      setOpen(true)
                     }}
-                    href='https://vimeo.com/127203262'
+                    href="#"
                   >
-                    <div className='vid-butn'>
-                      <span className='icon'>
-                        <i className='fas fa-play'></i>
+                    <div className="vid-butn">
+                      <span className="icon">
+                        <i className="fas fa-play"></i>
                       </span>
                     </div>
                   </a>
                 </div>
 
-                <div className='cont'>
-                  <h3 className='wow' data-splitting>
+                <div className="cont">
+                  <h3 className="wow" data-splitting>
                     Get to know us better
                   </h3>
                 </div>
               </div>
             </div>
-            <div className='col-lg-5 offset-lg-1'>
-              <div className='testim-box'>
-                <div className='head-box'>
-                  <h6 className='wow fadeIn' data-wow-delay='.5s'>
+            <div className="col-lg-5 offset-lg-1">
+              <div className="testim-box">
+                <div className="head-box">
+                  <h6 className="wow fadeIn" data-wow-delay=".5s">
                     What makes us diffrent
                   </h6>
-                  <h4 className='wow fadeInLeft' data-wow-delay='.5s'>
+                  <h4 className="wow fadeInLeft" data-wow-delay=".5s">
                     Why us ?
                   </h4>
                 </div>
                 <Slider
                   {...settings}
-                  className='slic-item wow fadeInUp'
-                  data-wow-delay='.5s'
+                  className="slic-item wow fadeInUp"
+                  data-wow-delay=".5s"
                 >
-                  <div className='item'>
+                  <div className="item">
                     <p>
                       We guide you through the online cosmos and accompany you
                       all through to your business goal. We help make you and
@@ -99,7 +99,7 @@ const VideoWithTestimonials = () => {
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default VideoWithTestimonials;
+export default VideoWithTestimonials
