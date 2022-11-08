da1bad694e417221e006ee200045f24e5b0cf5fd
Baricho <ekabue0@gmail.com>
2022-11-08 13:13:42 +0300

    changes

--- .next/static/chunks/pages/home/home1-light-c927a7b0dd71734e.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4],{5699:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/home/home1-light",function(){return u(2215)}])}},function(n){n.O(0,[277,654,598,648,857,215,774,888,179],(function(){return _=5699,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
